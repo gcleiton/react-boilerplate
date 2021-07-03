800db53db1df78cd330a893092c077228f6fe19e
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2021-07-03 16:52:16 -0300

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": [
+    "../src/presentation/**/*.stories.(ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ]
+}
