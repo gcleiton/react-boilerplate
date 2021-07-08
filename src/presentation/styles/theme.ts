dafc9a6a1f64104eac4c82fbb4a02bd540254f3b
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2021-07-08 08:41:41 -0300

    feat: improve home styles with theme variables

--- src/presentation/styles/theme.ts
+++ src/presentation/styles/theme.ts
@@ -1,3 +1,5 @@
+import { keyframes } from 'styled-components'
+
 export default {
   font: {
     families: {
@@ -8,10 +10,11 @@ export default {
     normal: 400,
     bold: 600,
     sizes: {
+      xxsmall: '.4rem',
       xsmall: '.6rem',
       small: '.8rem',
       medium: '1rem',
-      large: '1.2rem',
+      large: '1.3rem',
       xlarge: '1.6rem',
       xxlarge: '2rem',
       huge: '3rem'
@@ -34,6 +37,8 @@ export default {
 
   colors: {
     primary: '#03103b',
+    gradientPrimary: 'rgba(64, 219, 216, 0.15)',
+    gradientSecondary: 'rgba(3, 16, 59, 0.15)',
     blue: '#288ad6',
     white: '#ffffff',
     green: '#16c98d',
@@ -45,10 +50,30 @@ export default {
     gray: '#95a5a6'
   },
 
+  spacings: {
+    xxsmall: '.15rem',
+    xsmall: '.3rem',
+    small: '.6rem',
+    medium: '1rem',
+    large: '1.4rem',
+    xlarge: '1.8rem',
+    xxlarge: '2.4rem',
+    huge: '3rem'
+  },
+
   transitions: {
     default: 'all 330ms cubic-bezier(0.685, 0.0473, 0.346, 1)',
     roundTrip: 'width 450ms cubic-bezier(0.25, 1, 0.5, 1)',
     color: 'color 450ms cubic-bezier(0.25, 1, 0.5, 1)',
     transform: 'transform 250ms cubic-bezier(0.25, 1, 0.5, 1)'
+  },
+
+  keyframes: {
+    spin: keyframes`
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    `
   }
 }
